191a7cf97146f85ad48a9076a942c43d95b3ab51
Thanakorn Buathongtanakarn <scientan@gmail.com>
2024-05-30 09:41:11 +0700

    Patch AruCO

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -23,6 +23,7 @@ public class YourService extends KiboRpcService {
         movetopos(10.95, -9.9228d, 5.195, 0, 0, -0.70f, 0.70f, 3, true);
         targetack_debug(1, "item_cv", "bmptar1", "mattar1");
         // Move to the Target 2 position.
+        movetopos(0, 0, 0, 0, 0, 0, 0, 0, false);
     }
 
     @Override
@@ -37,15 +38,14 @@ public class YourService extends KiboRpcService {
 
     private void movetopos(double pos_x, double pos_y, double pos_z,
             double qua_x, double qua_y, double qua_z,
-            double qua_w, int recheckม, boolean log_enable) {
-        final int loop_max = recheck;
+            double qua_w, int recheck, boolean log_enable, int targetpos) {
         final Point point = new Point(pos_x, pos_y, pos_z);
         final Quaternion quaternion = new Quaternion((float) qua_x, (float) qua_y, (float) qua_z, (float) qua_w);
         Result result = api.moveTo(point, quaternion, true);
 
         int loopcount = 0;
         while (((Result) result).hasSucceeded() == false) {
-            if (loopcount >= loop_max) {
+            if (loopcount >= recheck) {
                 break;
             }
             result = api.moveTo(point, quaternion, true);
@@ -53,12 +53,12 @@ public class YourService extends KiboRpcService {
         }
 
         if (log_enable == true) {
-            log.logger("Flag1" + "Move to position: " + pos_x + ", " + pos_y + ", " + pos_z);
+            log.logger("Target" + targetpos + "Reached" + "Move to position: " + pos_x + ", " + pos_y + ", " + pos_z);
         }
     }
 
-    private void targetack_debug(int areadID, string item_name, string bitmapdebug_name, string matimgdebug_name) {
-        Mat image = getMatNavCam();
+    private void targetack_debug(int areaID, string item_name, string bitmapdebug_name, string matimgdebug_name) {
+        Mat image = api.getMatNavCam();
         api.saveBitmap(image, bitmapdebug_name);
         api.saveMatImage(image, matimgdebug_name);
         api.setAreaInfo(areaID, item_name);
@@ -67,7 +67,27 @@ public class YourService extends KiboRpcService {
         api.takeTargetItemSnapshot();
     }
 
-    private void aruco() {
+    private void objdetect() {
 
+    }
+
+    public void AruCO(double pos_x, double pos_y, double pos_z,
+            double qua_x, double qua_y, double qua_z,
+            double qua_w, int recheck, boolean log_enable) {
+        Mat ids = new Mat();
+        Mat source = api.getMatNavCam();
+        int arv = 0;
+        while (arv == 0) {
+            movetopos(pos_x, pos_y, pos_z, qua_x, qua_y, qua_z, qua_w, recheck, log_enable, 99);
+            Dictionary format = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
+            List<Mat> corners = new ArrayList<>();
+            try {
+                Aruco.detectMarkers(source, format, corners, ids);
+                arv = (int) ids.get(0, 0)[0];
+            } catch (Exception ec) {
+
+            }
+        }
+        api.judgeSendDiscoveredAR(Integer.toString(arv));
     }
 }
