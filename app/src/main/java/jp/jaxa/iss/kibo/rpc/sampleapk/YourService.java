a3b06196c088b109a9faa0f1de4dc9843540dcd7
Thanakorn Buathongtanakarn <scientan@gmail.com>
2024-06-02 00:17:22 +0700

    chore: Refactor YourService class, add movetopos method, and update Gradle Wrapper fix : debug, fixing bug, fix androidmanifestxml not found error. (root cause : Unknown)
    Signed-off-by: Thanakorn Buathongtanakarn <scientan@gmail.com>

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -1,13 +1,16 @@
 package jp.jaxa.iss.kibo.rpc.sampleapk;
 
+import android.graphics.Bitmap;
+
 import gov.nasa.arc.astrobee.Result;
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 
 import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
-import gov.nasa.arc.astrobee.types.Vec3d;
 
 import org.opencv.core.Mat;
+import java.util.logging.Logger;
+import java.util.logging.Logger;
 
 /**
  * Class meant to handle commands from the Ground Data System and execute them
@@ -23,7 +26,9 @@ public class YourService extends KiboRpcService {
         movetopos(10.95, -9.9228d, 5.195, 0, 0, -0.70f, 0.70f, 3, true);
         targetack_debug(1, "item_cv", "bmptar1", "mattar1");
         // Move to the Target 2 position.
-        movetopos(0, 0, 0, 0, 0, 0, 0, 0, false);
+        movetopos(11.235, -9.25, 5.295, 0.707, 0.0, 0.707, 0.00, 3, true);
+        movetopos(10.925, -8.875, 4.2, 0.500, 0.500, 0.500, 0.500, 3, false);
+        targetack_debug(2, "item_2", "bmptar2", "mattar2");
     }
 
     @Override
@@ -38,7 +43,7 @@ public class YourService extends KiboRpcService {
 
     private void movetopos(double pos_x, double pos_y, double pos_z,
             double qua_x, double qua_y, double qua_z,
-            double qua_w, int recheck, boolean log_enable, int targetpos) {
+            double qua_w, int recheck, boolean log_enable) {
         final Point point = new Point(pos_x, pos_y, pos_z);
         final Quaternion quaternion = new Quaternion((float) qua_x, (float) qua_y, (float) qua_z, (float) qua_w);
         Result result = api.moveTo(point, quaternion, true);
@@ -53,13 +58,15 @@ public class YourService extends KiboRpcService {
         }
 
         if (log_enable == true) {
-            log.logger("Target" + targetpos + "Reached" + "Move to position: " + pos_x + ", " + pos_y + ", " + pos_z);
+            Logger logger = Logger.getLogger(YourService.class.getName());
+            logger.info("Move to position: " + point.toString() + ", Quaternion: " + quaternion.toString());
         }
     }
 
-    private void targetack_debug(int areaID, string item_name, string bitmapdebug_name, string matimgdebug_name) {
+    private void targetack_debug(int areaID, String item_name, String bitmapdebug_name, String matimgdebug_name) {
         Mat image = api.getMatNavCam();
-        api.saveBitmap(image, bitmapdebug_name);
+        Bitmap bmpimage = api.getBitmapNavCam();
+        api.saveBitmapImage(bmpimage, bitmapdebug_name);
         api.saveMatImage(image, matimgdebug_name);
         api.setAreaInfo(areaID, item_name);
         api.reportRoundingCompletion();
@@ -67,27 +74,4 @@ public class YourService extends KiboRpcService {
         api.takeTargetItemSnapshot();
     }
 
-    private void objdetect() {
-
-    }
-
-    public void AruCO(double pos_x, double pos_y, double pos_z,
-            double qua_x, double qua_y, double qua_z,
-            double qua_w, int recheck, boolean log_enable) {
-        Mat ids = new Mat();
-        Mat source = api.getMatNavCam();
-        int arv = 0;
-        while (arv == 0) {
-            movetopos(pos_x, pos_y, pos_z, qua_x, qua_y, qua_z, qua_w, recheck, log_enable, 99);
-            Dictionary format = Aruco.getPredefinedDictionary(Aruco.DICT_5X5_250);
-            List<Mat> corners = new ArrayList<>();
-            try {
-                Aruco.detectMarkers(source, format, corners, ids);
-                arv = (int) ids.get(0, 0)[0];
-            } catch (Exception ec) {
-
-            }
-        }
-        api.judgeSendDiscoveredAR(Integer.toString(arv));
-    }
 }
